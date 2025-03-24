bd888eee60d4690d6c5400114ac63f973f20a22f
Anderson VTWNDS <andersonventosilla0@gmail.com>
2025-03-24 16:11:38 -0500

    i18n astro added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en", "br"],
+  },
   prefetch: true,
   vite: {
     plugins: [tailwindcss()]
---
+++ src/i18n/ui.ts
@@ -0,0 +1,30 @@
+export const languages = {
+    es: "Español",
+    en: 'English',
+    br: 'Portuguese',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        'nav.about': 'Nosotros',
+        'nav.product': 'Producto',
+        'nav.pricing': 'Precios',
+        'nav.contact': 'Contacto'
+    },
+    en: {
+        'nav.about': 'About',
+        'nav.product': 'Product',
+        'nav.pricing': 'Pricing',
+        'nav.contact': 'Contact'
+    },
+    br: {
+        'nav.about': 'Nós',
+        'nav.product': 'Produto',
+        'nav.pricing': 'Preços',
+        'nav.contact': 'Contato'
+    },
+} as const;
+
+export const showDefaultLang = false;
