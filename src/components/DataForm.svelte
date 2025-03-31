810905b7a7f24bcdf01faa06fcd92264964b2d44
Anderson VTWNDS <andersonventosilla0@gmail.com>
2025-03-31 17:53:15 -0500

    form with svelte added

--- src/components/DataForm.svelte
+++ src/components/DataForm.svelte
@@ -117,7 +117,7 @@
         <label
             class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1"
             for="email"
-        ></label>
+        >Email</label>
         <input
             type="email"
             id="email"
